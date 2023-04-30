b5e66e37d1d06a0f5525bc35dac75c6b1fe4bc1f
Luciano M. Smith <lsmith@fi.uba.ar>
2023-04-30 19:32:24 +0000

    hola

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-int main()
+int main()  // comentario main
 {
-    DigitalIn gasDetector(D2);
+    DigitalIn gasDetector(D2);  // constructor ??
 
-    DigitalOut alarmLed(LED1);
+    DigitalOut alarmLed(LED1);  // constructor ??
 
-    gasDetector.mode(PullDown);
+    gasDetector.mode(PullDown);  // método ??
 
-    alarmLed = OFF;
+    alarmLed = OFF; // método
+
+    bool alarm = false;
+
+    printf("%s\n", "Hello World");
 
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
-        }
-        
-        if ( gasDetector == OFF ) {
+            printf("%s\n", "Alarma activada");
+        } else {
             alarmLed = OFF;
-        }
+            printf("%s\n", "Alarma desactivada");
+        }        
     }
 }
